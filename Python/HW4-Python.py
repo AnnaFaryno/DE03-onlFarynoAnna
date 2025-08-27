3b14bcf95635a71bdc74c0c8b61d6957f1234633
MNS-FARYNO\astonuser <afaryno@mail.ru>
2025-08-27 12:47:12 +0300

    Исправления в HW4

--- Python/HW4-Python.py
+++ Python/HW4-Python.py
@@ -64,7 +64,7 @@ print("Только уникальные возраста: ", unique_age)
 list_1 = [1, 2, 3]
 list_2 = [4, 5, 6]
 
-list_3 = list_1 + list_2
+list_3 = list_1 + list_2 
 
 print("Объединенный список:", list_3)
 
